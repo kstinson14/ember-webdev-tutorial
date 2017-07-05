63119680846b23fd84b335a5ae97909a9e1f6ea1
Katrina Stinson <katrina.m.stinson@gmail.com>
2017-07-05 16:38:42 -0400

    Added acceptance / integration tests for map functionality

--- super-rentals/tests/acceptance/list-rentals-test.js
+++ super-rentals/tests/acceptance/list-rentals-test.js
@@ -1,7 +1,21 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
 
-moduleForAcceptance('Acceptance | list rentals');
+import Ember from 'ember';
+
+let StubMapsService = Ember.Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
 
 test('should show rentals as the home page', function (assert) {
   visit('/');
